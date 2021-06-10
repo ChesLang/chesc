ff309f6cb7eb67547e8345f69564619a2819c186
Garnet3106 <manage@gant.work>
2021-06-11 08:09:32 +0900

    add: cmp subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    rustnutc::cmd::run_command();
+    let mut cmd_procs = std::collections::HashMap::<String, fn(String, std::collections::HashMap::<String, Vec<String>>)>::new();
+
+    cmd_procs.insert("cmp".to_string(), chesc_cmp);
+
+    rustnutc::cmd::run_command(cmd_procs);
+}
+
+fn chesc_cmp(subcmd_name: String, subcmd_options: std::collections::HashMap<String, Vec<String>>) {
+    println!("- command data -");
+    println!("{}", subcmd_name);
+    println!("{:?}", subcmd_options);
 }
