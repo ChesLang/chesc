5667b170fee895fa83fe94185331e548c13ce513
Garnet3106 <manage@gant.work>
2021-06-14 12:51:14 +0900

    add: procedure of console

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 use rustnutc::cmd::*;
 use rustnutc::compiler::compiler::*;
+use rustnutc::console::*;
 
 fn main() {
-    let mut cmd_procs = std::collections::HashMap::<String, fn(String, std::collections::HashMap::<String, Vec<String>>)>::new();
+    let mut cmd_procs = std::collections::HashMap::<String, fn(String, std::collections::HashMap::<String, Vec<String>>, &mut Console)>::new();
 
     cmd_procs.insert("cmp".to_string(), chesc_cmp);
 
     run_command(cmd_procs);
 }
 
-fn chesc_cmp(subcmd_name: String, subcmd_options: std::collections::HashMap<String, Vec<String>>) {
+fn chesc_cmp(subcmd_name: String, subcmd_options: std::collections::HashMap<String, Vec<String>>, cons: &mut Console) {
     println!("- command data -");
     println!("{}", subcmd_name);
     println!("{:?}", subcmd_options);
 
+    let show_details = subcmd_options.contains_key("-det");
+
     let input_paths = match subcmd_options.get("-i") {
         Some(v) => v,
         None => {
-            println!("no input file");
-            return;
+            cons.log(ConsoleLogData::new(ConsoleLogKind::Error, "{^chesc.err.6805}", vec![], vec![format!("{{^chesc.usage}}: {{^chesc.usage.specify_input_file}}")]), show_details);
+            panic!("{}", Console::get_terminate_msg());
         }
     };
 
     if input_paths.len() == 0 {
-        println!("no input file");
-        return;
+        cons.log(ConsoleLogData::new(ConsoleLogKind::Error, "{^chesc.err.6805}", vec![], vec![format!("{{^chesc.usage}}: {{^chesc.usage.specify_input_file}}")]), show_details);
+        panic!("{}", Console::get_terminate_msg());
     }
 
     if input_paths.len() > 1 {
-        println!("too many files");
-        return;
+        cons.log(ConsoleLogData::new(ConsoleLogKind::Error, "{^chesc.err.6805}", vec![], vec![format!("{{^chesc.help}}: {{^chesc.help.cannot_specify_multiple_files}}")]), show_details);
+        panic!("{}", Console::get_terminate_msg());
     }
 
     let ref_dir_paths: Vec<String> = match subcmd_options.get("-ref") {
