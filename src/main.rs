5ec58753f84bd536c6ffe381a541acb3e3e215d3
Garnet3106 <manage@gant.work>
2021-06-15 20:40:17 +0900

    add: subfile loader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rustnutc::cmd::*;
 use rustnutc::compiler::compiler::*;
 use rustnutc::console::*;
+use rustnutc::fileman::*;
 
 fn main() {
     let mut cmd_procs = std::collections::HashMap::<String, fn(String, std::collections::HashMap::<String, Vec<String>>, &mut Console)>::new();
@@ -10,10 +11,10 @@ fn main() {
     run_command(cmd_procs);
 }
 
-fn chesc_cmp(subcmd_name: String, subcmd_options: std::collections::HashMap<String, Vec<String>>, cons: &mut Console) {
-    println!("- command data -");
-    println!("{}", subcmd_name);
-    println!("{:?}", subcmd_options);
+fn chesc_cmp(_subcmd_name: String, subcmd_options: std::collections::HashMap<String, Vec<String>>, cons: &mut Console) {
+    // println!("- command data -");
+    // println!("{}", subcmd_name);
+    // println!("{:?}", subcmd_options);
 
     let show_details = subcmd_options.contains_key("-det");
 
@@ -35,10 +36,27 @@ fn chesc_cmp(subcmd_name: String, subcmd_options: std::collections::HashMap<Stri
         panic!("{}", Console::get_terminate_msg());
     }
 
-    let ref_dir_paths: Vec<String> = match subcmd_options.get("-ref") {
+    let mut ref_dir_paths: Vec<String> = match subcmd_options.get("-ref") {
         Some(v) => v.clone(),
         None => vec![],
     };
+
+    // メインソースファイルを持つディレクトリを参照先に指定する
+    let abs_main_src_path = match FileMan::get_parent_dir_path(&input_paths.get(0).unwrap()) {
+        Ok(v) => {
+            if v.is_none() {
+                cons.log(ConsoleLogData::new(ConsoleLogKind::Error, "{^chesc.err.2711}", vec![], vec![]), show_details);
+            }
+
+            v.unwrap()
+        },
+        Err(e) => {
+            cons.log(e.get_log_data(), show_details);
+            panic!("{}", Console::get_terminate_msg());
+        },
+    };
+
+    ref_dir_paths.push((*abs_main_src_path).to_str().unwrap().to_string());
 
     let mut cmp = Compiler::new(input_paths.get(0).unwrap().clone(), ref_dir_paths, CompilerOutputKind::Executable);
 
